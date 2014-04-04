97ee648d03ffd79358bd089ab9d85fec21d220cb
Tiago Pinto <tiagoavpinto@hotmail.com>
2014-04-04 16:42:56 +0100

    apagados

--- trello/app/model/Task.js
+++
@@ -1,4 +0,0 @@
-Ext.define('TR.model.User', {
-    extend: 'Ext.data.Model',
-    fields: ['position', 'title', 'description']
-});
--- trello/app/store/Tasks.js
+++
@@ -1,9 +0,0 @@
-Ext.define('TR.store.Users', {
-    extend: 'Ext.data.Store',
-    fields: ['position', 'title', 'description'],
-    data: [
-        {position: '1',    title: 'Tarefa 1', description: 'Fazer XPTO'},
-          {position: '2',    title: 'Tarefa 2', description: 'Fazer XPTO2'},
-          {position: '3',    title: 'Tarefa 3', description: 'Fazer XPTO3'},
-    ]
-});
