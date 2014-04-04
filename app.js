f9f937e63d5e1fc2458ddee841a959c37a17dd21
Tiago Pinto <tiagoavpinto@hotmail.com>
2014-04-04 17:27:12 +0100

    app actualizado

--- app.js
+++ app.js
@@ -1,46 +1,12 @@
 Ext.application({
     requires: ['Ext.container.Viewport'],
-    name: 'AM',
+    name: 'TR',
 
     appFolder: 'app',
     
     controllers: [
-        'Users',
+        'Tasks',
     ],
-
-    launch: function() {
-        Ext.create('Ext.container.Viewport', {
-            title: "Trello",
-            layout: {
-                type: 'border',
-                //align: 'stretch'
-            },
-            renderTo: document.body,
-            items: [{
-                region: 'north',
-                xtype: 'label',
-                text: 'Isto é o header'
-            }, {
-                region: 'center',
-                xtype: 'userlist',
-                title: 'Painel com users',
-                flex: 2
-            }, {
-                region: 'west',
-                xtype: 'userlist',
-                title: 'Inner Panel Two',
-                flex: 1
-            }, {
-                region: 'east',
-                xtype: 'userlist',
-                title: 'Inner Panel Three',
-                flex: 1
-            }, {
-                region: 'south',
-                xtype: 'button',
-                text: 'Save',
-                action: 'save'
-            }]
-        });
-    }
+    
+    autoCreateViewport: true
 });
