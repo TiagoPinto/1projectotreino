9cc5559fbaadeacca36b4b34ba58c4ce67a40480
Tiago Pinto <tiagoavpinto@hotmail.com>
2014-04-16 15:50:06 +0100

    improved code

--- app/controller/Login.js
+++ app/controller/Login.js
@@ -6,19 +6,19 @@ Ext.define('TR.controller.Login', {
     views: ['Login.Login', 'Column.ListColumn'],
 
     refs: [{
-        ref: 'btnlogin',
-        selector: '#btnlogin'
+            ref: 'btnlogin',
+            selector: '#btnlogin'
     }, {
-        ref: 'viewport',
-        selector: 'viewport'
+            ref: 'viewport',
+            selector: 'viewport'
     }, {
-        ref: "columnContainer",
-        selector: "listcolumn [name=listtaskcontainer]"
-        },{
-        ref:'login',
-        selector:'#log'
+            ref: "columnContainer",
+            selector: "listcolumn [name=listtaskcontainer]"
+        }, {
+            ref: 'login',
+            selector: '#log'
         }
-          
+
           ],
 
     init: function () {
@@ -34,12 +34,12 @@ Ext.define('TR.controller.Login', {
 
     login: function (grid, record) {
         var viewport = this.getViewport();
-        var form= viewport.down('login').down('form');
-        var username=form.items.get(0).value;
-        var password=form.items.get(1).value;
-        
+        var form = viewport.down('login').down('form');
+        var username = form.items.get(0).value;
+        var password = form.items.get(1).value;
+
         var storeuser = Ext.StoreManager.get('Users');
-        
+
         for (var i = 1; i <= storeuser.data.length; i++) {
             if (storeuser.data.get(i).data.username == username && storeuser.data.get(i).data.password == password) {
                 var flag = 'true';
@@ -59,16 +59,7 @@ Ext.define('TR.controller.Login', {
             this.getLogin().removeAll();
             viewport.add(listColumn);
         } else {
-                Ext.Msg.alert('Login Failed','User or password incorrect');
-
-            
+            Ext.Msg.alert('Login Failed', 'User or password incorrect');
         }
-
-
-
-
-
-
-
     }
 });
